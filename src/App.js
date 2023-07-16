cf39a860ec5d4108c621d8d5ba3edad6ecd55796
Andrewphanpuspo <andrewphanpuspo2000@gmail.com>
2023-07-16 14:19:38 +1000

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Route, Routes } from "react-router-dom";
+import { Signin } from "./page/signIn/Signin";
+import { Signup } from "./page/signup/Signup";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/new-admin" element={<Signup />} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
